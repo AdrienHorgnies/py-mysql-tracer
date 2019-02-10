2426a6d373e8de49bc4ad0d3223d94b93a1d459f
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-02-10 21:20:30 +0100

    expose Query

--- tests/python/test_Query.py
+++ tests/python/test_Query.py
@@ -1,8 +1,8 @@
-import query
+from mysql_tracer import Query
 
 
 def test_query_str():
-    tested_query = query.Query('tests/assets/sample-query.sql')
+    tested_query = Query('tests/assets/sample-query.sql')
 
     assert tested_query.query_str == "SELECT name, title FROM person LEFT JOIN job " \
                                      "ON person.job_id = job.id WHERE title NOT IN ('developer');"
