e9591a5404cb15c7a43aa8bf8f50729070161b99
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-02-17 12:18:40 +0100

    move writing logic to its own module

--- tests/python/test_query.py
+++ tests/python/test_query.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
 import mock
-import os.path
 
 import query
 
@@ -23,7 +22,7 @@ def test_template_query_str():
 
 @mock.patch('query.CursorProvider')
 @mock.patch('query.datetime')
-def test_result(mock_datetime, mock_cp):
+def test_result(mock_datetime, mock_cp, query_path):
     mock_datetime.now.side_effect = (datetime(1992, 3, 4, 11, 0, 5, 654321),
                                      datetime(1992, 3, 4, 11, 0, 5, 987654))
 
@@ -35,52 +34,13 @@ def test_result(mock_datetime, mock_cp):
     ]
     mock_cursor.description = (('name',), ('title',))
 
-    actual = query.Query('tests/assets/sample-query.sql').result
+    actual = query.Query(query_path).result
 
     assert actual.execution_start == datetime(1992, 3, 4, 11, 0, 5, 654321)
     assert actual.execution_end == datetime(1992, 3, 4, 11, 0, 5, 987654)
     assert actual.duration == timedelta(microseconds=333333)
-    assert actual.rows == mock_cursor.fetchall.return_value
+    assert actual.rows == [
+        ('Adrien Horgnies', 'analyst developer'),
+        ('Constance de Lannoy', 'secretary')
+    ]
     assert actual.description == ('name', 'title')
-
-
-@mock.patch('query.CursorProvider')
-@mock.patch('query.datetime')
-def test_report(mock_datetime, mock_cp, query_path, executed_query_path):
-    mock_datetime.now.side_effect = (datetime(1992, 3, 4, 11, 0, 5, 654321),
-                                     datetime(1992, 3, 4, 11, 0, 5, 987654))
-
-    mock_cursor = mock.Mock()
-    mock_cp.cursor.return_value = mock_cursor
-    mock_cursor.fetchall.return_value = [
-        ('Adrien Horgnies', 'analyst developer'),
-        ('Constance de Lannoy', 'secretary')
-    ]
-    mock_cursor.description = (('name',), ('title',))
-
-    tested_query = query.Query(query_path)
-
-    assert os.path.basename(tested_query.report) == '1992-03-04T11-00-05_query.sql'
-    assert os.path.isfile(tested_query.report)
-    assert [line for line in open(tested_query.report)] == [line for line in open(executed_query_path)]
-
-
-@mock.patch('query.CursorProvider')
-@mock.patch('query.datetime')
-def test_export(mock_datetime, mock_cp, query_path, executed_export_path):
-    mock_datetime.now.side_effect = (datetime(1992, 3, 4, 11, 0, 5, 654321),
-                                     datetime(1992, 3, 4, 11, 0, 5, 987654))
-
-    mock_cursor = mock.Mock()
-    mock_cp.cursor.return_value = mock_cursor
-    mock_cursor.fetchall.return_value = [
-        ('Adrien Horgnies', 'analyst developer'),
-        ('Constance de Lannoy', 'secretary')
-    ]
-    mock_cursor.description = (('name',), ('title',))
-
-    tested_query = query.Query(query_path)
-
-    assert os.path.basename(tested_query.export) == '1992-03-04T11-00-05_query.csv'
-    assert os.path.isfile(tested_query.export)
-    assert [line for line in open(tested_query.export)] == [line for line in open(executed_export_path)]
---
+++ tests/python/test_writer.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from os.path import isfile, basename
+
+import mock
+import pytest
+
+import writer
+
+
+@pytest.fixture
+def query(query_path):
+    query_mock = mock.MagicMock()
+    query_mock.source = query_path
+    query_mock.result = mock.MagicMock()
+    query_mock.result.execution_start = datetime(1992, 3, 4, 11, 0, 5, 654321)
+    query_mock.result.execution_end = datetime(1992, 3, 4, 11, 0, 5, 987654)
+    query_mock.result.duration = timedelta(microseconds=333333)
+    query_mock.result.rows = [
+        ('Adrien Horgnies', 'analyst developer'),
+        ('Constance de Lannoy', 'secretary')
+    ]
+    query_mock.result.description = ('name', 'title')
+    return query_mock
+
+
+def test_write(query, executed_query_path, executed_export_path):
+    report, export = writer.write(query)
+
+    assert basename(report) == '1992-03-04T11-00-05_query.sql'
+    assert isfile(report)
+    assert [line for line in open(report)] == [line for line in open(executed_query_path)]
+    
+    assert basename(export) == '1992-03-04T11-00-05_query.csv'
+    assert isfile(export)
+    assert [line for line in open(export)] == [line for line in open(executed_export_path)]
