bfe27a238a3a891b6f5520d127b242165e477cc3
Adrien Horgnies <aHorgnies@altissia.com>
2019-05-06 09:54:30 +0200

    rearrange comments and code

--- mysql_tracer/__init__.py
+++ mysql_tracer/__init__.py
@@ -1,6 +1,6 @@
 # noinspection PyUnresolvedReferences
-# noinspection PyUnresolvedReferences
 from mysql_tracer._configuration import auto_configure
+# noinspection PyUnresolvedReferences
 from mysql_tracer._query import Query
 # noinspection PyUnresolvedReferences
 from mysql_tracer._query import Result
