3f1832f37362b23138219487bde0fa76a948102f
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2019-08-01 16:56:35 +0200

    define function to search for configuration file

--- mysql_tracer/_configuration.py
+++ mysql_tracer/_configuration.py
@@ -1,7 +1,7 @@
 import argparse
 import logging
 import os.path
-import re as __re
+from os import access, R_OK, X_OK
 from pathlib import Path
 
 import yaml
@@ -14,6 +14,28 @@ __configuration = None
 __configured_help = '\nThis item is configured under {keys} with value `{value}`.' \
                     '\nSetting this option will override this value.'
 __configurable_help = '\nThis item can be configured under {keys}.'
+
+
+def __find_config(config_name):
+    current_dir = Path.cwd()
+    while access(str(current_dir), X_OK) and current_dir.parent is not current_dir:
+        config_path = current_dir.joinpath(config_name)
+        if config_path.exists():
+            log.debug('Found configuration file %s', config_path)
+            if access(str(config_path), R_OK):
+                return config_path
+            else:
+                raise PermissionError(config_path)
+        current_dir = current_dir.parent
+
+    if __user_config_path.exists():
+        if access(str(__user_config_path), R_OK):
+            return __user_config_path
+        else:
+            raise PermissionError(__user_config_path)
+
+    log.debug('Did not find configuration file')
+    return None
 
 
 def __configure_yaml():
@@ -30,7 +52,7 @@ __configure_yaml()
 
 
 def __get_file_configuration(path=__user_config_path):
-    if not os.path.exists(path):
+    if not os.path.exists(str(path)):
         log.debug('No configuration file found, constructing configuration from zero')
         return dict()
     with open(str(path)) as config_file:
